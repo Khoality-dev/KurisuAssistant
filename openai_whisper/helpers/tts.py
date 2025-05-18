5026a91f4d8287fd4736e7469e50121a7af26edc
Khoa <khoakg1707@gmail.com>
2025-05-18 14:14:15 -0700

    change openai-whisper container to kurisu assistant core and move tts to core

--- openai_whisper/helpers/tts.py
+++ openai_whisper/helpers/tts.py
@@ -1,16 +1,15 @@
 import os
 import requests
-import dotenv
 
-dotenv.load_dotenv()
 
 class TTS:
     def __init__(self):
         self.api_url = os.getenv("TTS_API_URL", "http://127.0.0.1:9880/tts")
-
+        print(f"TTS API URL: {self.api_url}")
     def __call__(self, text: str) -> bytes:
         try:
             params = {
+                "text": text,
                 "text_lang": "ja",
                 "ref_audio_path": "reference/ayaka_ref.wav",
                 "prompt_lang": "ja",
@@ -20,6 +19,7 @@ class TTS:
                 "streaming_mode": True,
             }
             response = requests.get(self.api_url, params=params)
+            print(f"TTS Response: {response}")
             response.raise_for_status()
         except Exception as e:
             print(f"TTS Error: {e}")
