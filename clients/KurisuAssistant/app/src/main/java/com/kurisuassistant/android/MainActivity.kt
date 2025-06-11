f4c03fe1fb7a656096867af62651a213746752f7
Khoa <59268707+Khoality-dev@users.noreply.github.com>
2025-06-10 23:45:14 -0700

    Ensure chat input visible when keyboard opens

--- clients/KurisuAssistant/app/src/main/java/com/kurisuassistant/android/MainActivity.kt
+++ clients/KurisuAssistant/app/src/main/java/com/kurisuassistant/android/MainActivity.kt
@@ -9,11 +9,8 @@ import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kurisuassistant.android.silerovad.SileroVadOnnxModel
@@ -36,13 +33,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         Util.checkPermissions(this)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
